c55bcd69bc90cd6720c878e3f2c4ea953652c58e
Muhsin345 <mzmuhzin123@gmail.com>
2021-08-25 13:04:39 +0530

    upd

--- tstprjct/settings.py
+++ tstprjct/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'tstprjct.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'tstapp/TEMPLATES'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
