01ec7177162cb1bd536366c8f3b1fa7d6f0afbc2
Muhsin345 <mzmuhzin123@gmail.com>
2021-09-15 22:38:59 +0530

    nw

--- tstapp/urls.py
+++ tstapp/urls.py
@@ -6,5 +6,9 @@ urlpatterns=[
     path('Details/',views.fnDetails),
     path('Userhome/',views.fnUser),
     path('fbhome/',views.fnFB),
-    path('prdcts/',views.fnprdcts)
+    path('prdcts/',views.fnprdcts),
+    path('bootstrap/',views.fnnav),
+    path('gridnav/',views.fngrid),
+    path('facebook/',views.facebook),
+    path('sample/',views.navsmpl)
 ]
--- tstapp/views.py
+++ tstapp/views.py
@@ -12,4 +12,11 @@ def fnFB(request):
     return render(request,'fb345.html')
 def fnprdcts(request):
     return render(request,'prdcts.html')    
-
+def fnnav(req):
+    return render(req,'nav.html')  
+def fngrid(req):
+    return render(req,'gridnav.html')
+def facebook(req):
+    return render(req,'facebook.html')
+def navsmpl(req):
+    return render(req,'navsmpl.html')
